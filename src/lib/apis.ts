812728156058f7015c954324a05509cb02a94f45
fine-pine <lsm3645@g.skku.edu>
2025-09-18 23:43:11 +0900

    Add api functions

--- src/lib/apis.ts
+++ src/lib/apis.ts
@@ -1,15 +1,58 @@
-// TODO axios or fetch
 import axios from "axios";
-import { UnclassifiedTask } from "./type";
+import { NoteType, TaskType } from "./type";
 
-export function extractTasks(note: string, callback: (tasks: UnclassifiedTask[]) => void) {
-    
+export async function extractTasks(note: string, callback: (tasks: TaskType[]) => void) {
     return axios
-    .post("http://localhost:8080/api/categorizing/text", {
+    .post("https://port-0-oneminutes-mf68say742c07b64.sel5.cloudtype.app/api/categorizing/text", {
     content: note,
     })
     .then(function (response) {
-        callback(response.data.todos_by_person as UnclassifiedTask[]);
+        callback(response.data.todos_by_person as TaskType[]);
+    })
+    .catch(function (error) {
+    console.log(error);
+    });
+}
+
+export async function getTasks(callback: (tasks: TaskType[]) => void) {
+    return axios
+    .get("https://port-0-oneminutes-mf68say742c07b64.sel5.cloudtype.app/api/todo")
+    .then(function (response) {
+        callback(response.data.todos as TaskType[]);
+    })
+    .catch(function (error) {
+    console.log(error);
+    });
+}
+
+export async function saveTasks(tasks: TaskType[], callback?: (tasks: TaskType[]) => void) {
+    return axios
+    .post("https://port-0-oneminutes-mf68say742c07b64.sel5.cloudtype.app/api/todo", { todos: tasks })
+    .then(function (response) {
+        callback?.(response.data.todos as TaskType[]);
+    })
+    .catch(function (error) {
+    console.log(error);
+    });
+}
+
+export async function updateTask(task: TaskType, callback?: (task: TaskType) => void) {
+    return axios
+    .put(`https://port-0-oneminutes-mf68say742c07b64.sel5.cloudtype.app/api/todos/${task.id}`, task)
+    .then(function (response) {
+        callback?.(response.data.todo as TaskType);
+    })
+    .catch(function (error) {
+    console.log(error);
+    });
+}
+
+
+export async function getNotes(callback: (notes: NoteType[]) => void) {
+    return axios
+    .get(`https://port-0-oneminutes-mf68say742c07b64.sel5.cloudtype.app/api/meeting`)
+    .then(function (response) {
+        callback?.(response.data.meeting as NoteType[]);
     })
     .catch(function (error) {
     console.log(error);
